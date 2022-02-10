ed658c83e620e7a77686eb589f00a31ca881aefd
Fankesyooni <qzmmcn@163.com>
2022-02-10 18:53:30 +0800

    更新版本协议到 A-GPL3.0

--- app/src/main/java/com/fankes/tsbattery/utils/Utils.kt
+++ app/src/main/java/com/fankes/tsbattery/utils/Utils.kt
@@ -1,20 +1,22 @@
-/**
- * Copyright (C) 2022. Fankes Studio(qzmmcn@163.com)
+/*
+ * TSBattery - A new way to save your battery avoid cancer apps hacker it.
+ * Copyright (C) 2019-2022 qzmmcn@163.com
+ * https://github.com/fankes/TSBattery
  *
- * This file is part of TSBattery.
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by ferredoxin.
  *
- * TSBattery is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * TSBattery is distributed in the hope that it will be useful,
+ * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
  *
  * This file is Created by fankes on 2022/1/7.
  */
