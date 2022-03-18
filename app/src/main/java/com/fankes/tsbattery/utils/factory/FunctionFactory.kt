919d5bbd920aedeb2ea4b13d736eaed784c6b11a
Fankesyooni <qzmmcn@163.com>
2022-03-18 14:51:00 +0800

    Merge code

--- app/src/main/java/com/fankes/tsbattery/utils/factory/FunctionFactory.kt
+++ app/src/main/java/com/fankes/tsbattery/utils/factory/FunctionFactory.kt
@@ -80,17 +80,18 @@ val Context.versionName get() = packageInfo.versionName ?: ""
 val Context.versionCode get() = packageInfo.versionCode
 
 /**
- * dp 转换为 px
+ * dp 转换为 pxInt
+ * @param context 使用的实例
  * @return [Int]
  */
-val Number.dp get() = (toFloat() * appContext.resources.displayMetrics.density).toInt()
+fun Number.dp(context: Context) = dpFloat(context).toInt()
 
 /**
- * dp 转换为 px
+ * dp 转换为 pxFloat
  * @param context 使用的实例
  * @return [Float]
  */
-fun Number.dp(context: Context) = toFloat() * context.resources.displayMetrics.density
+fun Number.dpFloat(context: Context) = toFloat() * context.resources.displayMetrics.density
 
 /**
  * 跳转 APP 自身设置界面
