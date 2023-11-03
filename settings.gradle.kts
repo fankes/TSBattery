d4459aab405bef85020642c1512ee2b21e72b6b6
fankesyooni <qzmmcn@163.com>
2023-11-04 03:57:42 +0800

    chore: bump plugin versions
    - bump "com.highcapable.sweetdependency" version to 1.0.3
    - bump "com.highcapable.sweetproperty" version to 1.0.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,8 @@ pluginManagement {
     }
 }
 plugins {
-    id("com.highcapable.sweetdependency") version "1.0.2"
-    id("com.highcapable.sweetproperty") version "1.0.3"
+    id("com.highcapable.sweetdependency") version "1.0.3"
+    id("com.highcapable.sweetproperty") version "1.0.4"
 }
 sweetProperty {
     global {
